be18138ca0c2c295c9f8a3f48d99c5bf9cffa79c
Noella Umutoni <noellaumutoni@Noellas-MacBook-Air.local>
2025-04-26 06:15:54 +0200

    added tailwind configurationns

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./index.html",  // Ensure it includes the correct paths for your project
+      "./src/**/*.{js,ts,jsx,tsx}",  // Make sure it picks up all TypeScript files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
